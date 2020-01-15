6ff908b4b134c6a8d2f1c38a87dd53b5cdbca0bc
Paulo Henrique Silveira <paulohsilveira@outlook.com>
2020-01-14 23:57:50 -0300

    Adding instructions

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Change this at gatsby-config.js`,
+    description: `Change this at gatsby-config.js`,
+    author: `Change this at gatsby-config.js`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
