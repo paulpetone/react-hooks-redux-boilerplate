660f30afc12e26d09fd88cff0a637e52125a71bb
Paulo Henrique Silveira <paulohsilveira@outlook.com>
2020-01-20 22:12:11 -0300

    Activating redux dev tools only on dev mode

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -3,12 +3,13 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import itemsReducer from './reducers/itemsReducer'
 import requestsReducer from './reducers/requestsReducer'
 
+const isDev = process.env.NODE_ENV === 'development'
 const store = createStore(
   combineReducers({
     items: itemsReducer,
     requests: requestsReducer,
   }),
-  composeWithDevTools()
+  isDev ? composeWithDevTools() : undefined
 )
 
 export default store
